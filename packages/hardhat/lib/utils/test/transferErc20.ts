b06e6a7a254229f1dab43777208d57c2fbf45d00
Anthony Martin <anthony@replaycreative.com>
2021-11-27 17:02:23 +0100

    uses sendEth helper

--- packages/hardhat/lib/utils/test/transferErc20.ts
+++ packages/hardhat/lib/utils/test/transferErc20.ts
@@ -1,6 +1,6 @@
-import { parseUnits } from "ethers/lib/utils";
+import { parseEther, parseUnits } from "ethers/lib/utils";
 import { ethers } from "hardhat";
-import { getErc20, impersonateSigner } from ".";
+import { getErc20, impersonateSigner, sendEth } from ".";
 
 export const transferErc20 = async (
   token: string,
@@ -8,12 +8,9 @@ export const transferErc20 = async (
   to: string,
   amount: string
 ) => {
-  const [owner] = await ethers.getSigners();
-  await owner.sendTransaction({
-    to: from,
-    value: ethers.utils.parseEther("1.0"), // Sends exactly 1.0 ether
-  });
-
+  if ((await ethers.provider.getBalance(from)).lt(parseEther(".05"))) {
+    await sendEth(from, ".05");
+  }
   const erc20 = await getErc20(token, await impersonateSigner(from));
 
   return erc20.transfer(to, parseUnits(amount, await erc20.decimals()));
