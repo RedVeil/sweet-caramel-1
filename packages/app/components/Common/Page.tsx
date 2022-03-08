0abf01e4e0eb6e1dab70289035aac3085e6e1e3c
Leon Niesler <leon.niesler@gmail.com>
2022-03-08 19:00:36 +0100

    show mobile on prod

--- packages/app/components/Common/Page.tsx
+++ packages/app/components/Common/Page.tsx
@@ -1,15 +1,3 @@
-import MobileExcuseAlert from "components/MobileExcuseAlert";
-import { FeatureToggleContext } from "context/FeatureToggleContext";
-import { useContext } from "react";
-
 export default function Page(props) {
-  const { mobile: mobileEnabled } = useContext(FeatureToggleContext).features;
-  return mobileEnabled ? (
-    <>{props.children}</>
-  ) : (
-    <>
-      <div className="hidden lg:block">{props.children}</div>
-      <MobileExcuseAlert />
-    </>
-  );
+  return <>{props.children}</>;
 }
