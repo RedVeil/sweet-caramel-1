733eb804b2fde23ae5c689c6cf34e4474dbdc308
Amirjab21 <30866671+Amirjab21@users.noreply.github.com>
2022-04-12 14:49:31 +0100

    error handling for connecting wallet re-added (#589)

--- packages/app/hooks/useWeb3.ts
+++ packages/app/hooks/useWeb3.ts
@@ -1,10 +1,14 @@
 import { Web3Provider } from "@ethersproject/providers";
 import { getChainRelevantContracts } from "@popcorn/hardhat/lib/utils/getContractAddresses";
-import { useWeb3React } from "@web3-react/core";
+import { UnsupportedChainIdError, useWeb3React } from "@web3-react/core";
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected,
+} from "@web3-react/injected-connector";
 import walletSelectInterface from "components/WalletSelectInterface";
-import { setWalletSelectModal } from "context/actions";
+import { setSingleActionModal, setWalletSelectModal } from "context/actions";
 import { store } from "context/store";
-import { Wallets, walletToConnector } from "context/Web3/connectors";
+import { networkMap, Wallets, walletToConnector } from "context/Web3/connectors";
 import useWeb3Callbacks from "helper/useWeb3Callbacks";
 import { useCallback, useContext, useEffect, useMemo, useRef, useState } from "react";
 import useIsContractReady from "./useIsContractReady";
@@ -15,6 +19,19 @@ const getChain = (localstorage, envVar) => {
   }
   return parseInt(envVar || "4");
 };
+
+function getErrorMessage(error: Error) {
+  if (error instanceof NoEthereumProviderError) {
+    return "No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.";
+  } else if (error instanceof UnsupportedChainIdError) {
+    return `You're connected to an unsupported network. Please connect to ${networkMap[Number(process.env.CHAIN_ID)]}.`;
+  } else if (error instanceof UserRejectedRequestErrorInjected) {
+    return "Please authorize this website to access your Ethereum account.";
+  } else {
+    console.error(error);
+    return "An unknown error occurred. Check the console for more details.";
+  }
+}
 
 const getChainFromStorage = () => {
   return typeof localStorage !== "undefined" && localStorage?.getItem("chainId");
@@ -29,10 +46,27 @@ export default function useWeb3() {
   const cachedWallet = getWalletFromStorage();
   const [selectedWallet, setSelectedWallet] = useState<Wallets>(cachedWallet ? Number(cachedWallet) : Wallets.METAMASK);
 
-  const { library, account, chainId: _chainId, activate, deactivate } = useWeb3React<Web3Provider>();
+  const { library, account, chainId: _chainId, activate, deactivate, error } = useWeb3React<Web3Provider>();
 
   const ref = useRef(getChain(getChainFromStorage(), process.env.CHAIN_ID));
   const [chainId, setChainId] = useState(ref.current);
+
+  useEffect(() => {
+    if (error) {
+      dispatch(
+        setSingleActionModal({
+          content: getErrorMessage(error),
+          title: "Wallet Error",
+          visible: true,
+          type: "error",
+          onConfirm: {
+            label: "Close",
+            onClick: () => dispatch(setSingleActionModal(false)),
+          },
+        }),
+      );
+    }
+  }, [error]);
 
   const chooseWallet = useCallback(
     async (chosenWallet: number) => {
