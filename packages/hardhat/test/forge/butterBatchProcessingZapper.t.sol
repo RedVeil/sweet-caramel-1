f3fd058049b8696c750510da8e5be187b7be6f88
Amirjab21 <30866671+Amirjab21@users.noreply.github.com>
2022-06-03 13:22:50 +0100

    use ecmendenhall version of forge to avoid errors

--- packages/hardhat/test/forge/butterBatchProcessingZapper.t.sol
+++ packages/hardhat/test/forge/butterBatchProcessingZapper.t.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-import "forge-std/src/Test.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@ecmendenhall/ds-test/src/test.sol";
+import "@ecmendenhall/forge-std/src/Vm.sol";
+import { stdCheats } from "@ecmendenhall/forge-std/src/stdlib.sol";
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -27,12 +30,205 @@ address constant aclRegistryMainnet = 0x8A41aAa4B467ea545DDDc5759cE3D35984F093f4
 address constant butterProcessingMainnet = 0xCd979A9219DB9A353e29981042A509f2E7074D8B;
 address constant usdtMainnet = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
 
-contract ButterBatchProcessingZapperTest is Test {
-  using stdStorage for StdStorage;
-  StdStorage public test;
-  uint256 privateKey = 0xBEEF;
-  address owner = vm.addr(privateKey);
+struct StdStorage {
+  mapping(address => mapping(bytes4 => mapping(bytes32 => uint256))) slots;
+  mapping(address => mapping(bytes4 => mapping(bytes32 => bool))) finds;
+  bytes32[] _keys;
+  bytes4 _sig;
+  uint256 _depth;
+  address _target;
+  bytes32 _set;
+}
 
+library stdStorage {
+  event SlotFound(address who, bytes4 fsig, bytes32 keysHash, uint256 slot);
+  event WARNING_UninitedSlot(address who, uint256 slot);
+
+  Vm private constant vm_std_store = Vm(address(uint160(uint256(keccak256("hevm cheat code")))));
+
+  function sigs(string memory sigStr) internal pure returns (bytes4) {
+    return bytes4(keccak256(bytes(sigStr)));
+  }
+
+  /// @notice find an arbitrary storage slot given a function sig, input data, address of the contract and a value to check against
+  // slot complexity:
+  //  if flat, will be bytes32(uint256(uint));
+  //  if map, will be keccak256(abi.encode(key, uint(slot)));
+  //  if deep map, will be keccak256(abi.encode(key1, keccak256(abi.encode(key0, uint(slot)))));
+  //  if map struct, will be bytes32(uint256(keccak256(abi.encode(key1, keccak256(abi.encode(key0, uint(slot)))))) + structFieldDepth);
+  function find(StdStorage storage self) internal returns (uint256) {
+    address who = self._target;
+    bytes4 fsig = self._sig;
+    uint256 field_depth = self._depth;
+    bytes32[] memory ins = self._keys;
+
+    // calldata to test against
+    if (self.finds[who][fsig][keccak256(abi.encodePacked(ins, field_depth))]) {
+      return self.slots[who][fsig][keccak256(abi.encodePacked(ins, field_depth))];
+    }
+    bytes memory cald = abi.encodePacked(fsig, flatten(ins));
+    vm_std_store.record();
+    bytes32 fdat;
+    {
+      (, bytes memory rdat) = who.staticcall(cald);
+      fdat = bytesToBytes32(rdat, 32 * field_depth);
+    }
+
+    (bytes32[] memory reads, ) = vm_std_store.accesses(address(who));
+    if (reads.length == 1) {
+      bytes32 curr = vm_std_store.load(who, reads[0]);
+      if (curr == bytes32(0)) {
+        emit WARNING_UninitedSlot(who, uint256(reads[0]));
+      }
+      if (fdat != curr) {
+        require(false, "Packed slot. This would cause dangerous overwriting and currently isnt supported");
+      }
+      emit SlotFound(who, fsig, keccak256(abi.encodePacked(ins, field_depth)), uint256(reads[0]));
+      self.slots[who][fsig][keccak256(abi.encodePacked(ins, field_depth))] = uint256(reads[0]);
+      self.finds[who][fsig][keccak256(abi.encodePacked(ins, field_depth))] = true;
+    } else if (reads.length > 1) {
+      for (uint256 i = 0; i < reads.length; i++) {
+        bytes32 prev = vm_std_store.load(who, reads[i]);
+        if (prev == bytes32(0)) {
+          emit WARNING_UninitedSlot(who, uint256(reads[i]));
+        }
+        // store
+        vm_std_store.store(who, reads[i], bytes32(hex"1337"));
+        bool success;
+        bytes memory rdat;
+        {
+          (success, rdat) = who.staticcall(cald);
+          fdat = bytesToBytes32(rdat, 32 * field_depth);
+        }
+
+        if (success && fdat == bytes32(hex"1337")) {
+          // we found which of the slots is the actual one
+          emit SlotFound(who, fsig, keccak256(abi.encodePacked(ins, field_depth)), uint256(reads[i]));
+          self.slots[who][fsig][keccak256(abi.encodePacked(ins, field_depth))] = uint256(reads[i]);
+          self.finds[who][fsig][keccak256(abi.encodePacked(ins, field_depth))] = true;
+          vm_std_store.store(who, reads[i], prev);
+          break;
+        }
+        vm_std_store.store(who, reads[i], prev);
+      }
+    } else {
+      require(false, "No storage use detected for target");
+    }
+
+    require(self.finds[who][fsig][keccak256(abi.encodePacked(ins, field_depth))], "NotFound");
+
+    delete self._target;
+    delete self._sig;
+    delete self._keys;
+    delete self._depth;
+
+    return self.slots[who][fsig][keccak256(abi.encodePacked(ins, field_depth))];
+  }
+
+  function target(StdStorage storage self, address _target) internal returns (StdStorage storage) {
+    self._target = _target;
+    return self;
+  }
+
+  function sig(StdStorage storage self, bytes4 _sig) internal returns (StdStorage storage) {
+    self._sig = _sig;
+    return self;
+  }
+
+  function sig(StdStorage storage self, string memory _sig) internal returns (StdStorage storage) {
+    self._sig = sigs(_sig);
+    return self;
+  }
+
+  function with_key(StdStorage storage self, address who) internal returns (StdStorage storage) {
+    self._keys.push(bytes32(uint256(uint160(who))));
+    return self;
+  }
+
+  function with_key(StdStorage storage self, uint256 amt) internal returns (StdStorage storage) {
+    self._keys.push(bytes32(amt));
+    return self;
+  }
+
+  function with_key(StdStorage storage self, bytes32 key) internal returns (StdStorage storage) {
+    self._keys.push(key);
+    return self;
+  }
+
+  function depth(StdStorage storage self, uint256 _depth) internal returns (StdStorage storage) {
+    self._depth = _depth;
+    return self;
+  }
+
+  function checked_write(StdStorage storage self, address who) internal {
+    checked_write(self, bytes32(uint256(uint160(who))));
+  }
+
+  function checked_write(StdStorage storage self, uint256 amt) internal {
+    checked_write(self, bytes32(amt));
+  }
+
+  function checked_write(StdStorage storage self, bool write) internal {
+    bytes32 t;
+    assembly {
+      t := write
+    }
+    checked_write(self, t);
+  }
+
+  function checked_write(StdStorage storage self, bytes32 set) internal {
+    address who = self._target;
+    bytes4 fsig = self._sig;
+    uint256 field_depth = self._depth;
+    bytes32[] memory ins = self._keys;
+
+    bytes memory cald = abi.encodePacked(fsig, flatten(ins));
+    if (!self.finds[who][fsig][keccak256(abi.encodePacked(ins, field_depth))]) {
+      find(self);
+    }
+    bytes32 slot = bytes32(self.slots[who][fsig][keccak256(abi.encodePacked(ins, field_depth))]);
+
+    bytes32 fdat;
+    {
+      (, bytes memory rdat) = who.staticcall(cald);
+      fdat = bytesToBytes32(rdat, 32 * field_depth);
+    }
+    bytes32 curr = vm_std_store.load(who, slot);
+
+    if (fdat != curr) {
+      require(false, "Packed slot. This would cause dangerous overwriting and currently isnt supported");
+    }
+    vm_std_store.store(who, slot, set);
+    delete self._target;
+    delete self._sig;
+    delete self._keys;
+    delete self._depth;
+  }
+
+  function bytesToBytes32(bytes memory b, uint256 offset) public pure returns (bytes32) {
+    bytes32 out;
+
+    uint256 max = b.length > 32 ? 32 : b.length;
+    for (uint256 i = 0; i < max; i++) {
+      out |= bytes32(b[offset + i] & 0xFF) >> (i * 8);
+    }
+    return out;
+  }
+
+  function flatten(bytes32[] memory b) private pure returns (bytes memory) {
+    bytes memory result = new bytes(b.length * 32);
+    for (uint256 i = 0; i < b.length; i++) {
+      bytes32 k = b[i];
+      assembly {
+        mstore(add(result, add(32, mul(32, i))), k)
+      }
+    }
+
+    return result;
+  }
+}
+
+contract ButterBatchProcessingZapperTest is DSTest, stdCheats {
   MockERC20Permit Usdc;
   MockERC20 Usdt;
   MockERC20Permit Dai;
@@ -56,6 +252,12 @@ contract ButterBatchProcessingZapperTest is Test {
   uint256 usdtInitialUserBalance = 1000;
   uint256 daiInitialUserBalance = 785;
 
+  Vm public constant vm = Vm(HEVM_ADDRESS);
+  uint256 privateKey = 0xBEEF;
+  address owner = vm.addr(privateKey);
+  using stdStorage for StdStorage;
+  StdStorage public std_store_std_cheats;
+
   function setUp() public {
     // forks
     Usdc = MockERC20Permit(usdcMainnet);
@@ -70,7 +272,7 @@ contract ButterBatchProcessingZapperTest is Test {
     butterBatchProcessingZapper = new ButterBatchProcessingZapper(contractRegistry, mockCurveThreePool, _threeCrv);
     Usdt = new MockERC20("USD Tether", "UST", 6); // cant be forked as contract is not verified on etherscan
 
-    test //change the Usdt address on curve3Pool so it points at our deployed USDT contract
+    std_store_std_cheats //change the Usdt address on curve3Pool so it points at our deployed USDT contract
       .target(address(mockCurveThreePool))
       .sig(Curve3Pool(mockCurveThreePool).coins.selector)
       .with_key(2)
