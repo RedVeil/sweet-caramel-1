29ace9f9cb5a1ad3afb70a703b64c73b9d06181f
David Aji <davidenochaji@gmail.com>
2022-08-10 10:59:19 +0100

    update links in security section

--- packages/app/components/landing/SecuritySection.tsx
+++ packages/app/components/landing/SecuritySection.tsx
@@ -12,65 +12,85 @@ const SecuritySection = () => {
       </div>
       <div className="col-span-12 md:col-span-9 hidden md:grid grid-cols-3 gap-14 mt-9">
         <div className="col-span-3 md:col-span-1">
-          <div className=" bg-customRed rounded-lg p-14 h-52 md:h-80 w-full md:w-80 flex justify-center items-center xl:h-112 xl:w-full">
-            <img src="/images/zokyo.svg" alt="" className="rounded-lg" />
-          </div>
+          <a href="https://www.zokyo.io/audit-reports?2ebcbc23_page=3" target="_blank">
+            <div className=" bg-customRed rounded-lg p-14 h-52 md:h-80 w-full md:w-80 flex justify-center items-center xl:h-112 xl:w-full">
+              <img src="/images/zokyo.svg" alt="" className="rounded-lg" />
+            </div>
+          </a>
           <p className="text-black text-3xl leading-9 mt-6 mb-4">Zokyo</p>
-          <p className="text-primaryDark leading-5">
+          <a
+            href="https://assets.website-files.com/5f99eb79d508ca853be5f2e8/61b21474b7a1746d889f599d_Popcorn%20SC%20Audit.pdf"
+            target="_blank"
+            className="text-primaryDark leading-5"
+          >
             Zokyo is an end-to-end security resource that provides distinguishable security auditing and penetration
             testing services alongside prominent vulnerability assessments.
-          </p>
+          </a>
         </div>
 
         <div className="col-span-3 md:col-span-1">
-          <div className=" bg-customPurple rounded-lg p-14 h-52 md:h-80 w-full md:w-80 flex justify-center items-center xl:h-112 xl:w-full">
-            <img src="/images/g0.svg" alt="" className="rounded-lg" />
-          </div>
+          <a href="https://github.com/g0-group/Audits" target="_blank">
+            <div className=" bg-customPurple rounded-lg p-14 h-52 md:h-80 w-full md:w-80 flex justify-center items-center xl:h-112 xl:w-full">
+              <img src="/images/g0.svg" alt="" className="rounded-lg" />
+            </div>
+          </a>
           <p className="text-black text-3xl leading-9 mt-6 mb-4">g0</p>
-          <p className="text-primaryDark leading-5">
-            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-          </p>
+          <a href="/docs/PopcornMay2022.pdf" target="_blank" className="text-primaryDark leading-5">
+            g0 group is an industry leading security reviewer having audited blue chip blockchain projects such as
+            Gnosis Safe and Nexus Mutual
+          </a>
         </div>
 
         <div className="col-span-3 md:col-span-1">
-          <div className=" bg-customGreen rounded-lg p-14 h-52 md:h-80 w-full md:w-80 flex justify-center items-center xl:h-112 xl:w-full">
-            <img src="/images/immunefi.svg" alt="" className="rounded-lg" />
-          </div>
-          <p className="text-black text-3xl leading-9 mt-6 mb-4">Immunefi</p>
-          <p className="text-primaryDark leading-5">Immunefi is the leading bug bounty platform for Web3.</p>
+          <a href="https://immunefi.com/bounty/popcornnetwork/" target="_blank">
+            <div className=" bg-customGreen rounded-lg p-14 h-52 md:h-80 w-full md:w-80 flex justify-center items-center xl:h-112 xl:w-full">
+              <img src="/images/immunefi.svg" alt="" className="rounded-lg" />
+            </div>
+            <p className="text-black text-3xl leading-9 mt-6 mb-4">Immunefi</p>
+            <p className="text-primaryDark leading-5">Immunefi is the leading bug bounty platform for Web3.</p>
+          </a>
         </div>
       </div>
       <div className="col-span-12 md:hidden mt-9">
         <MobileCardSlider>
           <div className="px-2">
-            <div className=" bg-customRed rounded-lg p-14 h-80 w-full md:w-80 flex justify-center items-center">
-              <img src="/images/zokyo.svg" alt="" className="rounded-lg" />
-            </div>
+            <a href="https://www.zokyo.io/audit-reports?2ebcbc23_page=3" target="_blank">
+              <div className=" bg-customRed rounded-lg p-14  h-80 w-full md:w-80 flex justify-center items-center">
+                <img src="/images/zokyo.svg" alt="" className="rounded-lg" />
+              </div>
+            </a>
             <p className="text-black text-3xl leading-9 mt-6 mb-4">Zokyo</p>
-            <p className="text-primaryDark leading-5">
+            <a
+              href="https://assets.website-files.com/5f99eb79d508ca853be5f2e8/61b21474b7a1746d889f599d_Popcorn%20SC%20Audit.pdf"
+              target="_blank"
+              className="text-primaryDark leading-5"
+            >
               Zokyo is an end-to-end security resource that provides distinguishable security auditing and penetration
               testing services alongside prominent vulnerability assessments.
-            </p>
+            </a>
           </div>
 
           <div className="px-2">
-            <div className=" bg-customPurple rounded-lg p-14 h-80 w-full md:w-80 flex justify-center items-center">
-              <img src="/images/g0.svg" alt="" className="rounded-lg" />
-            </div>
+            <a href="https://github.com/g0-group/Audits" target="_blank">
+              <div className=" bg-customPurple rounded-lg p-14  h-80 w-full md:w-80 flex justify-center items-center">
+                <img src="/images/g0.svg" alt="" className="rounded-lg" />
+              </div>
+            </a>
             <p className="text-black text-3xl leading-9 mt-6 mb-4">g0</p>
-            <p className="text-primaryDark leading-5">
-              Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit Lorem ipsum
-              dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-            </p>
+            <a href="/docs/PopcornMay2022.pdf" target="_blank" className="text-primaryDark leading-5">
+              g0 group is an industry leading security reviewer having audited blue chip blockchain projects such as
+              Gnosis Safe and Nexus Mutual
+            </a>
           </div>
 
           <div className="px-2">
-            <div className=" bg-customGreen rounded-lg p-14 h-80 w-full md:w-80 flex justify-center items-center">
-              <img src="/images/immunefi.svg" alt="" className="rounded-lg" />
-            </div>
-            <p className="text-black text-3xl leading-9 mt-6 mb-4">Immunefi</p>
-            <p className="text-primaryDark leading-5">Immunefi is the leading bug bounty platform for Web3.</p>
+            <a href="https://immunefi.com/bounty/popcornnetwork/" target="_blank">
+              <div className=" bg-customGreen rounded-lg p-14 h-80 w-full md:w-80 flex justify-center items-center">
+                <img src="/images/immunefi.svg" alt="" className="rounded-lg" />
+              </div>
+              <p className="text-black text-3xl leading-9 mt-6 mb-4">Immunefi</p>
+              <p className="text-primaryDark leading-5">Immunefi is the leading bug bounty platform for Web3.</p>
+            </a>
           </div>
         </MobileCardSlider>
       </div>
