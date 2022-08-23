76d63ea07b2c24e7d7425bbf44b7cf362739c99d
David Aji <davidenochaji@gmail.com>
2022-08-24 00:54:27 +0100

    test:update product menu height

--- packages/app/components/NavBar/MobileProductsMenu.tsx
+++ packages/app/components/NavBar/MobileProductsMenu.tsx
@@ -6,7 +6,7 @@ import NavbarLink from "./NavbarLinks";
 const MobileProductsMenu = ({ onCloseMenu }) => {
   const router = useRouter();
   return (
-    <div className="h-screen px-6 py-12">
+    <div className="h-full px-6 py-12">
       <div className="relative">
         <ChevronLeftIcon
           className="text-black h-10 w-10 absolute -left-3 transform -translate-y-1/2 top-1/2"
