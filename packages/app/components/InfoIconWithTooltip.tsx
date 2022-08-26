6c1c44bae530b6a636e37407cb9663750cc7524d
David Aji <davidenochaji@gmail.com>
2022-08-26 13:12:02 +0100

    replace react-tooltip with rc-tooltip

--- packages/app/components/InfoIconWithTooltip.tsx
+++ packages/app/components/InfoIconWithTooltip.tsx
@@ -1,5 +1,6 @@
+import ReactTooltip from "rc-tooltip";
+import "rc-tooltip/assets/bootstrap.css";
 import React from "react";
-import ReactTooltip from "react-tooltip";
 
 export interface InfoIconWithTooltipProps {
   title?: string;
@@ -10,31 +11,27 @@ export interface InfoIconWithTooltipProps {
 
 export const InfoIconWithTooltip: React.FC<InfoIconWithTooltipProps> = ({ title, content, id, classExtras }) => {
   return (
-    <div className="flex items-center">
-      <img
-        src="/images/icons/tooltip.svg"
-        data-tip
-        data-for={id}
-        className={`cursor-pointer w-4 laptop:w-auto ${classExtras}`}
-      />
-      <ReactTooltip
-        id={id}
-        place={"bottom"}
-        effect="solid"
-        type="info"
-        className="shadow-lg rounded-lg border-2 border-customLightGray w-60"
-        backgroundColor="#fff"
-        borderColor="rgba(229, 231, 235, var(--tw-border-opacity))"
-        data-html="true"
-        border
-        delayHide={500}
-        getContent={() => (
-          <div className="text-black text-base leading-7">
-            <h6 className="mb-1">{title}:</h6>
-            <p className="text-primaryDark">{content}</p>
-          </div>
-        )}
-      />
-    </div>
+    // shadow-lg rounded-lg border-2 border-customLightGray
+    <ReactTooltip
+      id={id}
+      placement={"bottom"}
+      overlayClassName=" w-60"
+      data-html="true"
+      overlay={
+        <div className="text-black text-base leading-7">
+          <h6 className="mb-1">{title}:</h6>
+          <p className="text-primaryDark">{content}</p>
+        </div>
+      }
+    >
+      <div className="flex items-center">
+        <img
+          src="/images/icons/tooltip.svg"
+          data-tip
+          data-for={id}
+          className={`cursor-pointer w-4 laptop:w-auto ${classExtras}`}
+        />
+      </div>
+    </ReactTooltip>
   );
 };
