9173796757cdc0d21b99dda208ca980341064e7f
David Aji <davidenochaji@gmail.com>
2022-09-14 14:47:10 +0100

    update apply page to rebrand style

--- packages/grants/components/CommonComponents/Button.tsx
+++ packages/grants/components/CommonComponents/Button.tsx
@@ -18,7 +18,7 @@ const Button: React.FC<ButtonProps> = ({ variant, children, className, onClick, 
       case "secondary":
         if (disabled)
           return "bg-customLightGray border-customLightGray text-secondaryLight hover:border-customLightGray hover:bg-customLightGray hover:text-secondaryLight";
-        return "px-8 py-3 font-medium bg-white border border-primary text-primary hover:bg-primary hover:border-primary hover:text-white active:bg-white active:border-primary active:text-primary";
+        return "px-8 py-3 font-medium bg-transparent border border-primary text-primary hover:bg-primary hover:border-primary hover:text-white active:bg-white active:border-primary active:text-primary";
       case "tertiary":
         if (disabled) return "bg-gray-100 text-gray-500";
         return "bg-blue-100 border-blue-100 text-blue-600 hover:text-white hover:bg-blue-500 hover:border-blue-700 active:bg-blue-500 active:border-blue-500 active:text-white";
