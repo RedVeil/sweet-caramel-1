1750fff0f765c28463442310c90d0d6138eb5a4f
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-10-05 17:46:02 +0100

    profile page update #1289

--- packages/grants/components/Profile/AboutTab.tsx
+++ packages/grants/components/Profile/AboutTab.tsx
@@ -25,65 +25,55 @@ const AboutTab: React.FC<AboutTabProps> = ({ missionStatement, beneficiaryAddres
     };
   };
   return (
-    <div className={`grid grid-cols-12`}>
-      <div className="col-span-12 md:pb-28">
-        <h6 className=" text-gray-900 text-3xl mb-5">Overview</h6>
+    <ul>
+      <li className="pb-10">
+        <h6 className=" text-gray-900 text-3xl mb-5 font-semibold">Overview</h6>
         <p className=" text-primaryDark leading-6">{missionStatement || ""}</p>
-      </div>
-
-      <div className="col-span-12">
-        <div className={`grid md:gap-14 grid-cols-2 md:pb-20`}>
-          <div className={`col-span-2 md:col-span-1 mt-14 md:mt-0`}>
-            <h6 className=" text-black text-3xl leading-8 mb-4">Ethereum Address</h6>
-            <p className=" text-primaryDark leading-6 break-all">{beneficiaryAddress || ""}</p>
-          </div>
-
-          <div className={`col-span-2 md:col-span-1 mt-14 md:mt-0`}>
-            <h6 className=" text-black text-3xl leading-8 mb-4">Proof of Ownership</h6>
-            <a href={links?.proofOfOwnership} className=" text-primaryDark leading-6 break-all">
-              {links?.proofOfOwnership}
+      </li>
+      <li className="p-4 border border-customLightGray rounded-lg mb-2">
+        <h6 className="text-black text-base leading-6 font-semibold">Ethereum Address</h6>
+        <p className=" text-primaryDark leading-6 break-all">{beneficiaryAddress || ""}</p>
+      </li>
+      {links?.website && (
+        <li className="p-4 border border-customLightGray rounded-lg mb-2">
+          <h6 className=" text-black text-base leading-6 font-semibold">Website</h6>
+          <a href={links?.website} className=" text-primaryDark leading-6">
+            {links?.website}
+          </a>
+        </li>
+      )}
+      <li className="p-4 border border-customLightGray rounded-lg mb-2">
+        <h6 className=" text-black text-base leading-6 font-semibold">Proof of Ownership</h6>
+        <a href={links?.proofOfOwnership} className=" text-primaryDark leading-6 break-all">
+          {links?.proofOfOwnership}
+        </a>
+      </li>
+      <li className="p-4 border border-customLightGray rounded-lg">
+        <h6 className=" text-black text-base leading-6 font-semibold mb-2">Follow us on socials</h6>
+        <div className="flex space-x-4 md:space-x-6 items-center">
+          {links?.twitterUrl && (
+            <a href={links?.twitterUrl} className="flex justify-center items-center">
+              <TwitterIcon color="fill-primary" size="32" />
             </a>
-          </div>
-        </div>
-
-        <div className={`grid md:gap-14 grid-cols-2`}>
-          {links?.website && (
-            <div className={`col-span-2 md:col-span-1 mt-14 md:mt-0`}>
-              <h6 className=" text-black text-3xl leading-8 mb-4">Website</h6>
-              <a href={links?.website} className=" text-primaryDark leading-6">
-                {links?.website}
-              </a>
-            </div>
           )}
-
-          <div className={`col-span-2 md:col-span-1 mt-14 md:mt-0`}>
-            <h6 className=" text-black text-3xl leading-8 mb-4">Follow us on socials</h6>
-            <div className="flex gap-6 md:gap-8">
-              {links?.twitterUrl && (
-                <a href={links?.twitterUrl} className="flex justify-center items-center">
-                  <TwitterIcon color="fill-primary" size="32" />
-                </a>
-              )}
-              {links?.telegramUrl && (
-                <a href={links?.telegramUrl} className="flex justify-center items-center">
-                  <TelegramIcon color="fill-primary" size="32" />
-                </a>
-              )}
-              {links?.signalUrl && (
-                <a href={links?.signalUrl} className="flex justify-center items-center">
-                  <SignalIcon color="fill-primary" size="32" />
-                </a>
-              )}
-              {links?.linkedinUrl && (
-                <a href={links?.linkedinUrl} className="flex justify-center items-center">
-                  <LinkedInIcon color="fill-primary" size="32" />
-                </a>
-              )}
-            </div>
-          </div>
+          {links?.telegramUrl && (
+            <a href={links?.telegramUrl} className="flex justify-center items-center">
+              <TelegramIcon color="fill-primary" size="32" />
+            </a>
+          )}
+          {links?.signalUrl && (
+            <a href={links?.signalUrl} className="flex justify-center items-center">
+              <SignalIcon color="fill-primary" size="32" />
+            </a>
+          )}
+          {links?.linkedinUrl && (
+            <a href={links?.linkedinUrl} className="flex justify-center items-center">
+              <LinkedInIcon color="fill-primary" size="32" />
+            </a>
+          )}
         </div>
-      </div>
-    </div>
+      </li>
+    </ul>
   );
 };
 
