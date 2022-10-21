70a65029927e39290b60ec78307e0822e4c07767
shafu <selfouly@gmail.com>
2022-10-21 18:18:33 +0200

    refactor

--- packages/utils/src/multicallV3Encoder.ts
+++ packages/utils/src/multicallV3Encoder.ts
@@ -1,8 +1,34 @@
 const Web3 = require("web3");
+var Contract = require("web3-eth-contract");
 
 const TEST_VALUE = "65535";
+const EXAMPLE_ADDRESSES = ["0x6887246668a3b87F54DeB3b94Ba47a6f63F32985", "0xa4b1E63Cb4901E327597bc35d36FE8a23e4C253f"];
 
-let web3 = new Web3("https://goerli.infura.io/v3/86deaa6b3a2c4b19805ff8ac9051d403");
+const MULTICALLV3_ADDRESS = "0xcA11bde05977b3631167028862bE2a173976CA11";
+const MULTICALLV3_ABI = [
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: "address", name: "target", type: "address" },
+          { internalType: "bytes", name: "callData", type: "bytes" },
+        ],
+        internalType: "struct Multicall3.Call[]",
+        name: "calls",
+        type: "tuple[]",
+      },
+    ],
+    name: "aggregate",
+    outputs: [
+      { internalType: "uint256", name: "blockNumber", type: "uint256" },
+      { internalType: "bytes[]", name: "returnData", type: "bytes[]" },
+    ],
+    stateMutability: "payable",
+    type: "function",
+  },
+];
+
+let web3 = new Web3(process.env.RPC_URL);
 
 const depositEncoding = web3.eth.abi.encodeFunctionCall(
   {
@@ -32,5 +58,12 @@ const stakeEncoding = web3.eth.abi.encodeFunctionCall(
   [TEST_VALUE],
 );
 
-console.log(depositEncoding);
-console.log(stakeEncoding);
+var multicall = new Contract(MULTICALLV3_ABI, MULTICALLV3_ADDRESS);
+
+multicall.methods
+  .aggregate([
+    [EXAMPLE_ADDRESSES[0], depositEncoding],
+    [EXAMPLE_ADDRESSES[1], stakeEncoding],
+  ])
+  .call()
+  .then(console.log);
