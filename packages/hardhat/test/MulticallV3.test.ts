7c35fe76de328e9e8029e45695c25ac24d69348a
shafu <selfouly@gmail.com>
2022-10-25 15:58:00 +0200

    refactor

--- packages/hardhat/test/MulticallV3.test.ts
+++ packages/hardhat/test/MulticallV3.test.ts
@@ -17,20 +17,39 @@ import {
   VaultBlockLockHelper,
   VaultFeeController,
 } from "../typechain";
-import { DAYS, getErc20, impersonateSigner, sendEth, setTimestamp, timeTravel, transferErc20 } from "../lib/utils/test";
 import {
   getTransferFromEncoding,
   getStakeForEncoding,
   getDepositEncoding,
   getApproveEncoding,
 } from "../../utils/src/functionEncodings";
-const Web3 = require("web3");
+import { Contract } from "packages/utils/node_modules/ethers/lib";
 
-let web3 = new Web3(process.env.RPC_URL);
+// we only need aggregate from the multicall contract
+const MULTICALLV3_ABI = [
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: "address", name: "target", type: "address" },
+          { internalType: "bytes", name: "callData", type: "bytes" },
+        ],
+        internalType: "struct Multicall3.Call[]",
+        name: "calls",
+        type: "tuple[]",
+      },
+    ],
+    name: "aggregate",
+    outputs: [
+      { internalType: "uint256", name: "blockNumber", type: "uint256" },
+      { internalType: "bytes[]", name: "returnData", type: "bytes[]" },
+    ],
+    stateMutability: "payable",
+    type: "function",
+  },
+];
 
-const MINUTE = 60;
-const DAY = 60 * 60 * 24;
-const DEPOSIT_AMOUNT = parseEther("1000");
+const MULTICALLV3_CONTRACT = "0xcA11bde05977b3631167028862bE2a173976CA11";
 const FEE_MULTIPLIER = parseEther("0.0001"); // 1e14
 
 let owner: SignerWithAddress,
@@ -40,6 +59,7 @@ let owner: SignerWithAddress,
   rewardsManager: SignerWithAddress,
   zapper: SignerWithAddress;
 let contracts: Contracts;
+let multicall: Contract;
 
 interface Contracts {
   depositToken: MockERC20;
@@ -170,39 +190,14 @@ async function deployContracts(): Promise<Contracts> {
   };
 }
 
-// we only need aggregate from the multicall contract
-const MULTICALLV3_ABI = [
-  {
-    inputs: [
-      {
-        components: [
-          { internalType: "address", name: "target", type: "address" },
-          { internalType: "bytes", name: "callData", type: "bytes" },
-        ],
-        internalType: "struct Multicall3.Call[]",
-        name: "calls",
-        type: "tuple[]",
-      },
-    ],
-    name: "aggregate",
-    outputs: [
-      { internalType: "uint256", name: "blockNumber", type: "uint256" },
-      { internalType: "bytes[]", name: "returnData", type: "bytes[]" },
-    ],
-    stateMutability: "payable",
-    type: "function",
-  },
-];
-
-const MULTICALLV3_CONTRACT = "0xcA11bde05977b3631167028862bE2a173976CA11";
-
 describe("Vault", async () => {
-  it("deposit + stake", async () => {
+  beforeEach(async () => {
     [owner, depositor, depositor2, receiver, rewardsManager, zapper] = await ethers.getSigners();
     contracts = await deployContracts();
 
-    const multicall = await ethers.getContractAt(MULTICALLV3_ABI, MULTICALLV3_CONTRACT, owner);
-
+    multicall = await ethers.getContractAt(MULTICALLV3_ABI, MULTICALLV3_CONTRACT, owner);
+  });
+  it("deposit + stake", async () => {
     const DEPOSIT_AMOUNT = parseEther("100");
 
     await contracts.depositToken.mint(owner.address, DEPOSIT_AMOUNT);
@@ -210,15 +205,24 @@ describe("Vault", async () => {
     await contracts.depositToken.connect(owner).approve(multicall.address, DEPOSIT_AMOUNT);
 
     await multicall.aggregate([
+      // 1) transfer tokens from owner to multicall contract
       [contracts.depositToken.address, getTransferFromEncoding(owner.address, multicall.address, DEPOSIT_AMOUNT)],
+      // 2) approve the vault to spend the tokens of the multicall contract
       [contracts.depositToken.address, getApproveEncoding(contracts.vault.address, DEPOSIT_AMOUNT)],
+      // 3) deposit multicall tokens in vault
       [contracts.vault.address, getDepositEncoding(DEPOSIT_AMOUNT, multicall.address)],
+      // 4) approve the staking contract to spend the tokens of the multicall contract
       [contracts.vault.address, getApproveEncoding(contracts.staking.address, DEPOSIT_AMOUNT)],
+      // 5) stake vault shares of the multicall contract in staking contract
       [contracts.staking.address, getStakeForEncoding(DEPOSIT_AMOUNT, owner.address)],
     ]);
 
     // the owner should have his staking tokens now
     var balanceOfOwner = await contracts.staking.balanceOf(owner.address);
     expect(parseInt(balanceOfOwner._hex)).to.equal(parseInt(DEPOSIT_AMOUNT._hex));
+
+    // the multicall should have not staking tokens now
+    var balanceOfMulticall = await contracts.staking.balanceOf(multicall.address);
+    expect(parseInt(balanceOfMulticall._hex)).to.equal(0);
   });
 });
