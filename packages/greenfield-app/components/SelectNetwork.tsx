df06ef648f86439d56aa7db21df7a4cddd547a6e
David Aji <davidenochaji@gmail.com>
2022-11-11 13:33:26 +0100

    update network switcher ui

--- packages/greenfield-app/components/SelectNetwork.tsx
+++ packages/greenfield-app/components/SelectNetwork.tsx
@@ -1,22 +1,71 @@
-import { ChainId, networkLogos } from "@popcorn/utils"
+import { ChevronDownIcon } from "@heroicons/react/outline";
+import { ChainId, networkLogos, networkMap } from "@popcorn/utils"
+import Image from "next/image";
+import { useState } from "react";
+import { MobilePopupSelect } from "./MobilePopupSelect";
 
 interface SelectNetworkProps {
-  supportedNetworks: ChainId[];
-  selectedNetworks: ChainId[];
-  selectNetwork: (chainId: ChainId) => void;
+	supportedNetworks: ChainId[];
+	selectedNetworks: ChainId[];
+	selectNetwork: (chainId: ChainId) => void;
 }
 
 export default function SelectNetwork({ supportedNetworks, selectedNetworks, selectNetwork }: SelectNetworkProps): JSX.Element {
-  return (
-    <div className="flex flex-row items-center space-x-2">
-      <p className="text-lg mr-4">Selected Networks:</p>
-      {supportedNetworks.map(network =>
-        <button key={network}
-          onClick={() => selectNetwork(network)}
-          className={`${selectedNetworks.includes(network) ? "bg-warmGray" : "bg-white"} h-8 w-12 border border-customLightGray rounded-4xl text-primary cursor-pointer`}>
-          <img src={networkLogos[network]} alt={ChainId[network]} className="w-4.5 h-4 mx-auto" />
-        </button>
-      )}
-    </div>
-  )
+	const [openFilter, setOpenFilter] = useState(false);
+	const [categoryFilter, setCategoryFilter] = useState<{ id: string; value: string }>({ id: "1", value: "All" });
+	const switchFilter = (value: { id: any; value: any }) => {
+		selectNetwork(value.id);
+		setCategoryFilter(value)
+	};
+	const networkCategories = supportedNetworks.map(network => {
+		return {
+			id: network,
+			value: <div className="flex space-x-4">
+				<Image src={networkLogos[network]} alt={ChainId[network]} height="24px" width="24px" objectFit="contain" />
+				<p className="text-secondaryDark">{networkMap[network]}</p>
+			</div>
+		}
+	});
+	return (
+		<>
+			<div className="hidden md:flex flex-row items-center space-x-2 mb-8">
+				{supportedNetworks.map(network =>
+					<button key={network}
+						onClick={() => selectNetwork(network)}
+						className={`${selectedNetworks.includes(network) ? "bg-warmGray" : "bg-white"} h-14 w-18 border border-customLightGray rounded-3xl text-primary cursor-pointer flex justify-center items-center`}>
+						<Image src={networkLogos[network]} alt={ChainId[network]} height="24px" width="24px" objectFit="contain" />
+					</button>
+				)}
+			</div>
+
+			<div className="block md:hidden my-10">
+				<button
+					onClick={(e) => {
+						e.preventDefault();
+						setOpenFilter(true);
+					}}
+					className="w-full py-3 px-5 flex flex-row items-center justify-between mt-1 space-x-1 rounded-4xl border border-gray-300"
+				>
+					<div className="flex items-center">
+						{supportedNetworks.map(network =>
+							<div className="-ml-2 flex items-center" key={network}>
+								<Image src={networkLogos[network]} alt={ChainId[network]} height="32px" width="32px" objectFit="contain" />
+							</div>
+						)}
+					</div>
+					<ChevronDownIcon className="w-5 h-5" aria-hidden="true" />
+				</button>
+			</div>
+			<div className="no-select-dot absolute left-0">
+				<MobilePopupSelect
+					categories={networkCategories}
+					visible={openFilter}
+					onClose={setOpenFilter}
+					selectedItem={categoryFilter}
+					switchFilter={switchFilter}
+					title="Network filters"
+				/>
+			</div>
+		</>
+	)
 }
--- packages/greenfield-app/pages/staking.tsx
+++ packages/greenfield-app/pages/staking.tsx
@@ -7,39 +7,39 @@ import useChainsWithStaking from "hooks/staking/useChainsWithStaking";
 import SelectNetwork from "components/SelectNetwork";
 
 export default function StakingOverviewPage(): JSX.Element {
-  const stakingContracts = useAllStakingContracts();
-  const supportedNetworks = useChainsWithStaking()
-  const [selectedNetworks, selectNetwork] = useSelectNetwork(supportedNetworks)
+	const stakingContracts = useAllStakingContracts();
+	const supportedNetworks = useChainsWithStaking()
+	const [selectedNetworks, selectNetwork] = useSelectNetwork(supportedNetworks)
 
-  const stakingPools = useMemo(
-    () => stakingContracts?.stakingPools?.filter(staking => selectedNetworks.includes(staking?.chainId)),
-    [selectedNetworks, stakingContracts])
+	const stakingPools = useMemo(
+		() => stakingContracts?.stakingPools?.filter(staking => selectedNetworks.includes(staking?.chainId)),
+		[selectedNetworks, stakingContracts])
 
-  return (
-    <>
-      <div className="grid grid-cols-12">
-        <div className="col-span-12 md:col-span-4">
-          <h1 className=" text-5xl md:text-6xl leading-12">Staking</h1>
-          <p className="text-black mt-2">Earn more by staking your tokens</p>
-        </div>
-        <div className="col-span-12 md:col-span-6 md:col-end-13 mt-12 md:mt-0">
-          <ConnectDepositCard />
-        </div>
-      </div>
-      <SelectNetwork supportedNetworks={supportedNetworks} selectedNetworks={selectedNetworks} selectNetwork={selectNetwork} />
-      <div className="mt-12 border-t border-t-customLightGray">
-        <div className="w-full">
-          <div className="h-full ">
-            {stakingPools && stakingPools.length > 0 && stakingPools?.map(staking =>
-              <StakeCard
-                key={staking.chainId + staking.address}
-                chainId={staking?.chainId}
-                stakingAddress={staking?.address}
-                stakingType={staking?.stakingType}
-              />)}
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="grid grid-cols-12">
+				<div className="col-span-12 md:col-span-4">
+					<h1 className=" text-5xl md:text-6xl leading-12">Staking</h1>
+					<p className="text-black mt-2">Earn more by staking your tokens</p>
+				</div>
+				<div className="col-span-12 md:col-span-6 md:col-end-13 mt-12 md:mt-0">
+					<ConnectDepositCard />
+				</div>
+			</div>
+			<SelectNetwork supportedNetworks={supportedNetworks} selectedNetworks={selectedNetworks} selectNetwork={selectNetwork} />
+			<div className="border-t border-t-customLightGray">
+				<div className="w-full">
+					<div className="h-full ">
+						{stakingPools && stakingPools.length > 0 && stakingPools?.map(staking =>
+							<StakeCard
+								key={staking.chainId + staking.address}
+								chainId={staking?.chainId}
+								stakingAddress={staking?.address}
+								stakingType={staking?.stakingType}
+							/>)}
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
