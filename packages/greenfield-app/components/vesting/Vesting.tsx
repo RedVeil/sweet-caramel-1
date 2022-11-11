695961f759894634304b0ac7257023301ca00885
RedVeil <leon.niesler@gmail.com>
2022-11-11 14:41:41 +0100

    fixed reward double load, network icons in rewards, vesting loading animation

--- packages/greenfield-app/components/vesting/Vesting.tsx
+++ packages/greenfield-app/components/vesting/Vesting.tsx
@@ -33,55 +33,54 @@ export default function Vesting({ chainId }: VestingProps): JSX.Element {
       );
     }
   };
-  if (isValidating && !error) return (
-    <div className="my-4">
-      <ContentLoader viewBox="0 0 450 100" backgroundColor={"#EBE7D4"} foregroundColor={"#d7d5bc"}>
-        {/*eslint-disable */}
-        <rect x="0" y="0" rx="8" ry="8" width="450" height="100" />
-        {/*eslint-enable */}
-      </ContentLoader>
-    </div>
-  )
-
   return (
-    <div className={`flex flex-col h-full ${totalClaimablePop.eq(constants.Zero) ? "hidden" : ""}`}>
-      <div className="flex flex-row items-center mt-4">
-        <img src={networkLogos[chainId]} alt={ChainId[chainId]} className="w-4.5 h-4 mr-4" />
-        <p className="text-xl mt-0.5">{ChainId[chainId]}</p>
+    <>
+      <div className={`my-4 ${isValidating && totalClaimablePop.eq(constants.Zero) && !error ? '' : 'hidden'}`}>
+        <ContentLoader viewBox="0 0 450 100" backgroundColor={"#EBE7D4"} foregroundColor={"#d7d5bc"}>
+          {/*eslint-disable */}
+          <rect x="0" y="0" rx="8" ry="8" width="450" height="100" />
+          {/*eslint-enable */}
+        </ContentLoader>
       </div>
-      <div className="flex flex-col md:flex-row gap-8 md:gap-0 md:space-x-8 w-full mb-8 mt-4">
-        {totalVestingPop &&
-          <RewardSummaryCard
-            content={`${formatAndRoundBigNumber(totalVestingPop, 18)} POP`}
-            title={"Total Vesting"}
-            iconUri="/images/lock.svg"
-            infoIconProps={{
-              id: "Total Vesting",
-              title: "Total Vesting",
-              content:
-                "Every time you claim rewards a new 'Vesting Record' below will be added. Rewards in each 'Vesting Record' unlock over time. Come back periodically to claim new rewards as they unlock.",
-              classExtras: "h-5 w-5 ml-2",
-            }}
-          />
-        }
-        {totalClaimablePop &&
-          <RewardSummaryCard
-            content={`${formatAndRoundBigNumber(totalClaimablePop, 18)} POP`}
-            title={"Total Claimable"}
-            iconUri="/images/yellowCircle.svg"
-            button={true}
-            handleClick={() => claimAllEscrows()}
-            infoIconProps={{
-              id: "Total Claimable",
-              title: "Total Claimable",
-              content:
-                "This describes the total amount of Rewards that you can currently claim across all 'Vesting Records'.",
-              classExtras: "h-5 w-5 ml-2",
-            }}
-          />
-        }
+      <div className={`flex flex-col h-full ${!totalClaimablePop || totalClaimablePop.eq(constants.Zero) ? "hidden" : ""}`}>
+        <div className="flex flex-row items-center mt-4">
+          <img src={networkLogos[chainId]} alt={ChainId[chainId]} className="w-4.5 h-4 mr-4" />
+          <p className="text-xl mt-0.5">{ChainId[chainId]}</p>
+        </div>
+        <div className="flex flex-col md:flex-row gap-8 md:gap-0 md:space-x-8 w-full mb-8 mt-4">
+          {totalVestingPop &&
+            <RewardSummaryCard
+              content={`${formatAndRoundBigNumber(totalVestingPop, 18)} POP`}
+              title={"Total Vesting"}
+              iconUri="/images/lock.svg"
+              infoIconProps={{
+                id: "Total Vesting",
+                title: "Total Vesting",
+                content:
+                  "Every time you claim rewards a new 'Vesting Record' below will be added. Rewards in each 'Vesting Record' unlock over time. Come back periodically to claim new rewards as they unlock.",
+                classExtras: "h-5 w-5 ml-2",
+              }}
+            />
+          }
+          {totalClaimablePop &&
+            <RewardSummaryCard
+              content={`${formatAndRoundBigNumber(totalClaimablePop, 18)} POP`}
+              title={"Total Claimable"}
+              iconUri="/images/yellowCircle.svg"
+              button={true}
+              handleClick={() => claimAllEscrows()}
+              infoIconProps={{
+                id: "Total Claimable",
+                title: "Total Claimable",
+                content:
+                  "This describes the total amount of Rewards that you can currently claim across all 'Vesting Records'.",
+                classExtras: "h-5 w-5 ml-2",
+              }}
+            />
+          }
+        </div>
+        <div className="flex flex-col border-t border-customLightGray overflow-hidden" />
       </div>
-      <div className="flex flex-col border-t border-customLightGray overflow-hidden" />
-    </div>
+    </>
   )
 }
