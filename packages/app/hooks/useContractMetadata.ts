923ff7b9d226e36e2d7c3cb2e467f1c05f3f2154
Anthony Martin <anthony@replaycreative.com>
2022-11-11 01:22:13 -0500

    updates hook

--- packages/app/hooks/useContractMetadata.ts
+++ packages/app/hooks/useContractMetadata.ts
@@ -2,7 +2,7 @@ import { ChainId } from "@popcorn/utils";
 import { useMemo } from "react";
 import { useDeployment } from "@popcorn/app/hooks/useDeployment";
 
-export const useContractMetadata = (address: string, chainId: ChainId) => {
+export const useContractMetadata = (address: string, chainId: ChainId): { address: string; [key: string]: any } => {
   const addresses = useDeployment(chainId);
   return useMemo(() => {
     return {
