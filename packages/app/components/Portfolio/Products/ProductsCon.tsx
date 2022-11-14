ff6e305f3051bfec705a75c83beeab65c28fec2f
David Aji <davidenochaji@gmail.com>
2022-11-14 16:30:40 +0100

    created ui for sweet vaults portfolio

--- packages/app/components/Portfolio/Products/ProductsCon.tsx
+++ packages/app/components/Portfolio/Products/ProductsCon.tsx
@@ -1,12 +1,14 @@
 import PortfolioCardCon from "../PortfolioCardCon";
 import ButterProduct from "./ButterProduct";
 import StakingProduct from "./StakingProduct";
+import SweetVaultsProduct from "./SweetVaultsProduct";
 import ThreeXProduct from "./ThreeXProduct";
 
 const ProductsCon = () => {
   return (
     <PortfolioCardCon cardTitle="Products">
       <StakingProduct />
+      <SweetVaultsProduct />
       <ThreeXProduct />
       <ButterProduct />
     </PortfolioCardCon>
