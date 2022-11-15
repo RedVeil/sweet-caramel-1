d299af94abffa7fb33970ef9302c12819175701b
Anthony Martin <anthony@replaycreative.com>
2022-11-15 18:45:51 -0500

    adds circulating supply util

---
+++ packages/utils/src/circulating-supply/cache.ts
@@ -0,0 +1,19 @@
+import fs from "fs";
+import path from "path";
+export const cache = {
+  default: function () {
+    return { lastBlock: 0, holders: [] };
+  },
+  path: function (chainId) {
+    return path.resolve(__dirname, `${chainId}-holders.json`);
+  },
+  exists: function (chainId) {
+    return fs.existsSync(this.path(chainId));
+  },
+  get: function (chainId) {
+    return this.exists(chainId) ? JSON.parse(fs.readFileSync(this.path(chainId), "utf8")) : this.default();
+  },
+  write: function (holders, chainId, endBlock) {
+    fs.writeFile(this.path(chainId), JSON.stringify({ lastBlock: endBlock, holders }), () => {});
+  },
+};
