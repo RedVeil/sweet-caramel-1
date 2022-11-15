3de0692393058d4af0a0a23cbb87cf506065acd4
Kate Johnson <johnson.kat.g@gmail.com>
2022-11-15 15:07:47 -0500

    changed bytes32 to bytes in VaultsFactory deploy func

--- packages/hardhat/contracts/core/defi/vault/VaultsFactory.sol
+++ packages/hardhat/contracts/core/defi/vault/VaultsFactory.sol
@@ -37,16 +37,16 @@ contract VaultsFactory is Owned {
    * @param _implementation - address of contract to clone behavior code
    * @dev This should always be called through the VaultController
    */
-  function deploy(address _implementation, bytes32 calldata _args)
+  function deploy(address _implementation, bytes calldata _args)
     external
     onlyOwner
-    returns (address implementation, bytes32 memory returnData)
+    returns (address implementation, bytes memory returnData)
   {
     implementation = Clones.clone(implementation);
 
     bytes4 selector = implementation.initialize.selector;
 
-    (bool success, bytes32 memory returnData) = implementation.call(abi.encodeWithSelector(selector, _args));
+    (bool success, bytes memory returnData) = implementation.call(abi.encodeWithSelector(selector, _args));
 
     if (!success) revert DeploymentInitFailed();
 
