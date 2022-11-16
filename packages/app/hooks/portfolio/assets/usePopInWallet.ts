623dcad24ad7c3bdb374162e9ba8adcc6ac479fb
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-11-16 10:03:10 +0100

    update pop in wallet calculations

--- packages/app/hooks/portfolio/assets/usePopInWallet.ts
+++ packages/app/hooks/portfolio/assets/usePopInWallet.ts
@@ -9,43 +9,31 @@ import { useMemo } from "react";
 
 export default function usePopInWallet() {
   const { Ethereum, Polygon } = ChainId;
+  const ethereum = useDeployment(Ethereum);
+  const polygon = useDeployment(Polygon);
   const { account } = useWeb3();
 
   // calculate for ethereum
-  const { xPop: ethXPopAddress, pop: ethPopAddress } = useDeployment(Ethereum);
-
-  const ethPop = useERC20(ethPopAddress, Ethereum);
-  const ethBalancesPop = useBalanceAndAllowance(ethPop?.address, account, ethXPopAddress, Ethereum);
-
-  // alt calc for eth
-  const ethTokensPrice = useTokenPrices(ethPop?.address, Ethereum);
-  const EthPopValue = ethTokensPrice ? ethPop?.balance?.mul(ethTokensPrice) : numberToBigNumber(0, 18);
+  const { pop: ethereumPopAddress } = useDeployment(Ethereum);
+  const ethereumPop = useERC20(ethereumPopAddress, Ethereum);
+  const { data: ethereumLpPriceData } = useTokenPrices([ethereum.pop, ethereum.popUsdcArrakisVault], Polygon); // in 1e18
+  const ethereumLpPrice = ethereumLpPriceData?.[ethereum.popUsdcArrakisVault];
+  const ethereumPopValue = ethereumLpPrice ? ethereumPop?.balance?.mul(ethereumLpPrice) : numberToBigNumber(0, 18);
 
   // calculate for polygon
-  const { xPop: polygonXPopAddress, pop: polygonPopAddress } = useDeployment(Polygon);
-
+  const { pop: polygonPopAddress } = useDeployment(Polygon);
   const polygonPop = useERC20(polygonPopAddress, Polygon);
-  const polygonBalancesPop = useBalanceAndAllowance(polygonPop?.address, account, polygonXPopAddress, Polygon);
-
-  // alt calc for polygon
-  const polyTokensPrice = useTokenPrices(polygonPop?.address, Polygon);
-  const polygonPopValue = polyTokensPrice ? polygonPop?.balance?.mul(polyTokensPrice) : numberToBigNumber(0, 18);
-
-  // sum up the balances
-  const totalValue = useMemo(() => {
-    return BigNumber.from("0")
-      .add(ethBalancesPop?.balance || 0)
-      .add(polygonBalancesPop?.balance || 0);
-  }, [ethBalancesPop?.balance, polygonBalancesPop?.balance]);
+  const { data: poylgonLpPriceData } = useTokenPrices([polygon.pop, polygon.popUsdcArrakisVault], Polygon); // in 1e18
+  const polygonLpPrice = poylgonLpPriceData?.[polygon.popUsdcArrakisVault];
+  const polygonPopValue = polygonLpPrice ? polygonPop?.balance?.mul(polygonLpPrice) : numberToBigNumber(0, 18);
 
   const totalPop = useMemo(() => {
     return BigNumber.from("0")
       .add(polygonPopValue || 0)
-      .add(EthPopValue || 0);
-  }, [EthPopValue, polygonPopValue]);
+      .add(ethereumPopValue || 0);
+  }, [ethereumPopValue, polygonPopValue]);
 
   return {
-    totalValue,
     totalPop,
   };
 }
