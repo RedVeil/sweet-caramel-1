1dd4ba5e3a438c3549bb981d8355a2c1a0eebfda
shafu.eth <selfouly@gmail.com>
2022-11-16 14:51:49 +0100

    filter logs

--- packages/greenfield-app/pages/proxy.tsx
+++ packages/greenfield-app/pages/proxy.tsx
@@ -1,38 +1,13 @@
+import { useRpcProvider } from "@popcorn/app/hooks/useRpcProvider";
+import { Contract, ethers } from "ethers";
 import { useContractWrite, usePrepareContractWrite } from "wagmi";
 
 const PROXY_FACTORY_ADDRESS = "0xD5bFeBDce5c91413E41cc7B24C8402c59A344f7c";
 
 const PROXY_FACTORY_ABI = [
   {
-    constant: true,
-    inputs: [{ name: "", type: "address" }],
-    name: "isProxy",
-    outputs: [{ name: "", type: "bool" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "cache",
-    outputs: [{ name: "", type: "address" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
     constant: false,
     inputs: [],
-    name: "build",
-    outputs: [{ name: "proxy", type: "address" }],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [{ name: "owner", type: "address" }],
     name: "build",
     outputs: [{ name: "proxy", type: "address" }],
     payable: false,
@@ -60,6 +35,19 @@ export default function Proxy(): JSX.Element {
   });
 
   const { data, isSuccess, write, isLoading } = useContractWrite(config);
+
+  const provider = useRpcProvider(1337);
+  const contract = new Contract(PROXY_FACTORY_ADDRESS, PROXY_FACTORY_ABI, provider);
+  console.log(contract);
+
+  const aa = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+  const ff = contract.filters.Created(null, aa);
+  console.log(3333);
+  console.log(ff);
+
+  contract.queryFilter(ff).then((events) => {
+    console.log(events);
+  });
 
   return (
     <div>
