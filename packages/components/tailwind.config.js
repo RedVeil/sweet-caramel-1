1d1dc3ed29454fa43f6fcc02e60802bf9ad7b2b1
David Aji <davidenochaji@gmail.com>
2022-11-28 13:52:42 +0100

    add tailwindcss to storybook

---
+++ packages/components/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./pages/**/*.{ts,tsx}",
+		"./stories/**/*.{ts,tsx}",
+	],
+
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
