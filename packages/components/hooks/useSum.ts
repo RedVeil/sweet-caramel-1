719d04f719ddbe5b6657b6fdd5b7116efee690a6
Anthony Martin <anthony@replaycreative.com>
2022-11-29 19:30:30 +0100

    renames useSum for backwards compat

--- packages/components/hooks/useSum.ts
+++ packages/components/hooks/useSum.ts
@@ -1,68 +1,42 @@
-import { useEffect, useState, useMemo, useRef } from "react";
+import { useEffect, useState } from "react";
 import { BigNumber, constants } from "ethers";
-import useLog from "../pop/utils/hooks/useLog";
 
 export const useSum = ({
   expected,
-  timeout,
-  enabled,
 }: {
-  expected?: number;
-  timeout?: number; // in ms
-  enabled?: boolean; // undefined is true
-}): { loading: boolean; sum?: BigNumber; add: (amount: BigNumber) => void; reset: () => void } => {
-  const _enabled = enabled === undefined ? true && !!expected : enabled && !!expected;
-  const _expected = expected || 0;
-
-  const [loading, setLoading] = useState(!!_expected);
-  const count = useRef(0);
-  const sum = useRef(constants.Zero);
+  expected: number;
+}): { loading: boolean; sum: BigNumber; add: (amount: BigNumber) => void; reset: () => void } => {
+  const [sum, setSum] = useState(constants.Zero);
+  const [loading, setLoading] = useState(true);
+  const [count, setCount] = useState(0);
 
   const add = (amount?: BigNumber) => {
-    if (!_enabled) return;
-    if (!amount || !!_expected == false) return;
-    if (typeof _expected !== "number") return;
-    count.current++;
-    sum.current = sum.current.add(amount);
+    if (!amount) return;
+    setCount((count) => count + 1);
+    setSum((sum) => sum.add(amount));
   };
 
-  useEffect(() => {
-    if (!!enabled && !!_expected && count.current >= _expected) {
-      setLoading(false);
-    }
-  }, [_expected, count.current, _enabled]);
-
   const reset = () => {
-    count.current = 0;
-    sum.current = constants.Zero;
+    setCount((prevState) => 0);
+    setSum((prevState) => constants.Zero);
     setLoading((prevState) => true);
   };
 
   useEffect(() => {
-    if (!_enabled && !!timeout) return;
     const id = setTimeout(() => {
       setLoading(false);
-    }, timeout);
+    }, 100000);
     return () => {
       clearTimeout(id);
     };
-  }, [_enabled]);
+  }, []);
 
-  const finished = useMemo(() => {
-    return (loading && !!_expected && false) || (!loading && !!_expected && _expected >= count.current);
-  }, [_expected, count, loading]);
+  useEffect(() => {
+    if (count >= expected) {
+      setLoading(false);
+    }
+  }, [count]);
 
-  const response = useMemo(() => {
-    return { loading, sum: sum.current, add, reset };
-  }, [_enabled, finished, loading]);
-
-  useLog({ response, _enabled, finished, loading, _expected, count: count.current }, [
-    enabled,
-    finished,
-    loading,
-    expected,
-    count,
-  ]);
-  return response;
+  return { loading, sum, add, reset };
 };
 export default useSum;
