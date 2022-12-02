b408c353f27cd2f5c2ecb2ec5ccdf660d37a9faf
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-12-02 18:31:47 +0100

    chore: remove popcorn.network

--- packages/popcorn.network/components/Link.tsx
+++
@@ -1,5 +0,0 @@
-export const Link = ({href, children, target = "_blank", className = ''}) => {
-  return (
-    <a href={href} target={target} className={'text-customPurple hover:text-customPurpleLight leading-5' +' '+ className}>{children}</a>
-  );
-};
