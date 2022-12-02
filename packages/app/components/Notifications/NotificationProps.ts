a297eb38027ad0b99407dcb6f89ee9f939d7a758
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-12-02 17:50:26 +0100

    chore: remove app folder

--- packages/app/components/Notifications/NotificationProps.ts
+++
@@ -1,9 +0,0 @@
-export interface NotificationProps {
-  id: number;
-  visible: boolean;
-  type: "success" | "error" | "waiting";
-  title: string;
-  isFlash?: boolean;
-  content: string | JSX.Element;
-  backdrop?: boolean;
-}
