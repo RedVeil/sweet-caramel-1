a1ee6a40d6bba420872c2ecbd5a68975c3387f7e
oluwatunmiisheii <oluwatunmiisheii@users.noreply.github.com>
2022-12-06 10:10:23 +0000

    Prettified Code!

--- packages/components/pop/utils/hocs/withBigNumberFormatting.tsx
+++ packages/components/pop/utils/hocs/withBigNumberFormatting.tsx
@@ -4,14 +4,14 @@ import { Pop, BigNumberWithFormatted } from "../../types";
 
 export const withBigNumberFormatting =
   (Component: Pop.FC<BigNumberWithFormatted>) =>
-    ({ ...props }: Pop.StdProps & { data?: BigNumber }) => {
-      return (
-        <Component
-          {...props}
-          data={{
-            value: props.data,
-            formatted: props.data ? formatAndRoundBigNumber(props.data, 18) : undefined,
-          }}
-        />
-      );
-    };
+  ({ ...props }: Pop.StdProps & { data?: BigNumber }) => {
+    return (
+      <Component
+        {...props}
+        data={{
+          value: props.data,
+          formatted: props.data ? formatAndRoundBigNumber(props.data, 18) : undefined,
+        }}
+      />
+    );
+  };
