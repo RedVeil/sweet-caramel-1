5b7256c35a230fb79e24c8446c6b562fa107d74c
anthonymartin <anthonymartin@users.noreply.github.com>
2022-12-06 14:54:15 +0000

    Prettified Code!

--- packages/components/pop/Contract/Value.tsx
+++ packages/components/pop/Contract/Value.tsx
@@ -11,25 +11,25 @@ interface Props {
 }
 const eth_call =
   (Component: Pop.FC<any>) =>
-    ({
-      price,
-      balance,
-      decimals,
-      status,
-      callback,
-    }: Props & { status?: "idle" | "error" | "success" | "loading" } & { callback?: (value?: BigNumber) => void }) => {
-      const value =
-        (balance &&
-          price &&
-          balance
-            .mul(price)
-            .mul(parseUnits("1", decimals == 6 ? 12 : 0))
-            .div(parseUnits("1", 18))) ||
-        constants.Zero;
-      if (status === "success" && callback && value) {
-        callback(value);
-      }
-      return <Component {...{ price, balance, decimals, value, status }} />;
-    };
+  ({
+    price,
+    balance,
+    decimals,
+    status,
+    callback,
+  }: Props & { status?: "idle" | "error" | "success" | "loading" } & { callback?: (value?: BigNumber) => void }) => {
+    const value =
+      (balance &&
+        price &&
+        balance
+          .mul(price)
+          .mul(parseUnits("1", decimals == 6 ? 12 : 0))
+          .div(parseUnits("1", 18))) ||
+      constants.Zero;
+    if (status === "success" && callback && value) {
+      callback(value);
+    }
+    return <Component {...{ price, balance, decimals, value, status }} />;
+  };
 
 export const Value = eth_call(withLoading(({ value }) => <>{"$" + formatAndRoundBigNumber(value, 18)}</>));
