15a8ab9b5c6fef1d45f77052f6ee08f8dc43dc75
Anthony Martin <anthony@replaycreative.com>
2022-12-07 21:05:04 +0100

    reverts featureflag change

--- packages/components/defaultFeatureFlags.ts
+++ packages/components/defaultFeatureFlags.ts
@@ -2,7 +2,7 @@ const defaultFeatureFlags = [
   // Add Feature flags here
   { key: "showLocalNetwork", prod: false, dev: true },
   { key: "migrationAlert", prod: true, dev: true },
-  { key: "instant3X", prod: false, dev: true },
+  { key: "instant3X", prod: false, dev: false },
   { key: "sweetVaults", prod: false, dev: true },
   { key: "portfolio", prod: false, dev: true },
   { key: "optin_analytics", prod: false, dev: true },
@@ -12,9 +12,7 @@ const envEnabledDeafaultFeatureFlags: { [key: string]: boolean } = {};
 defaultFeatureFlags.forEach(
   (flag) =>
     (envEnabledDeafaultFeatureFlags[flag.key] =
-      !!process.env.NEXT_PUBLIC_IS_DEV || !!process.env.IS_DEV || !process.env.NODE_ENV?.includes("prod")
-        ? flag.dev
-        : flag.prod),
+      !!process.env.NEXT_PUBLIC_IS_DEV || !!process.env.IS_DEV ? flag.dev : flag.prod),
 );
 
 export default envEnabledDeafaultFeatureFlags;
