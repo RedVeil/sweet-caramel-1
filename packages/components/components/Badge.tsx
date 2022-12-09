06d2bdceb012a2eb11c4e0f84075d2b43867a243
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-12-09 16:25:34 +0100

    chore: create badge component

--- packages/components/components/Badge.tsx
+++ packages/components/components/Badge.tsx
@@ -1,22 +1,25 @@
-import { FC } from 'react'
+import { FC, ReactNode } from 'react'
 import classnames from "classnames";
 
 enum BadgeVariant {
   warning = 'warning',
   success = 'success',
+  white = 'white'
 }
 
 interface BadgeProps {
-  variant: BadgeVariant;
-  label: string
+  variant: keyof typeof BadgeVariant;
+  children: ReactNode
 }
 
-export const Badge: FC<BadgeProps> = ({ variant }) => {
+export const Badge: FC<BadgeProps> = ({ variant, children }) => {
   return (
-    <span className={classnames("inline-flex items-center rounded-full font-medium", {
-      'bg-customLightYellow text-yellow-800': variant === BadgeVariant.warning,
+    <span className={classnames("inline-flex items-center rounded-full font-medium text-black text-base py-3 px-5 border border-[#d7d7d799]", {
+      'bg-customLightYellow': variant === BadgeVariant.warning,
+      'bg-customLightGreen': variant === BadgeVariant.success,
+      'bg-white': variant === BadgeVariant.white,
     })}>
-      Badge
+      {children}
     </span>
   )
 }
