2b6b45e3f3b1055a7771b664e4b3cfda7223bcfb
anthonymartin <anthonymartin@users.noreply.github.com>
2022-12-09 13:51:35 +0000

    Prettified Code!

--- packages/components/pop/Staking/Apy.tsx
+++ packages/components/pop/Staking/Apy.tsx
@@ -4,21 +4,21 @@ import { useApy } from "./hooks";
 
 const eth_call =
   (Component: Pop.FC<BigNumberWithFormatted>) =>
-    ({ ...props }: Pop.BaseContractProps & { render?: ({ data, status, ...props }) => React.ReactElement<any> }) => {
-      const { data, status } = useApy({ ...props });
-      if (props.render) {
-        return (
-          <>
-            {props.render({
-              data: data,
-              status: status,
-              ...props,
-            })}
-          </>
-        );
-      }
-      return <Component {...props} data={data} status={status} />;
-    };
+  ({ ...props }: Pop.BaseContractProps & { render?: ({ data, status, ...props }) => React.ReactElement<any> }) => {
+    const { data, status } = useApy({ ...props });
+    if (props.render) {
+      return (
+        <>
+          {props.render({
+            data: data,
+            status: status,
+            ...props,
+          })}
+        </>
+      );
+    }
+    return <Component {...props} data={data} status={status} />;
+  };
 
 export const Apy = eth_call(withLoading(({ data }) => <>{data?.formatted || "0"}</>));
 
