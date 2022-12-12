ed75576230c18b920f2e4ffbd8d6aa0991ee9aab
David Aji <davidenochaji@gmail.com>
2022-12-12 11:04:32 +0100

    update tabs story

--- packages/components/stories/Tab.stories.tsx
+++ packages/components/stories/Tab.stories.tsx
@@ -1,40 +1,22 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import TabSwitcher, { TabPanel, Tab } from "../components/Tabs";
+import { Tabs } from "../components/Tabs";
 
 export default {
-  title: "Example/Tab",
-  component: TabSwitcher,
+  title: "Example/Tabs",
+  component: Tabs,
   argTypes: {
     backgroundColor: { control: "color" },
   },
-} as ComponentMeta<typeof TabSwitcher>;
+} as ComponentMeta<typeof Tabs>;
 
-const Template: ComponentStory<typeof TabSwitcher> = (args) => <TabSwitcher {...args} />;
-const TabPanelTemplate: ComponentStory<typeof TabPanel> = (args) => <TabPanel {...args} />;
+const Template: ComponentStory<typeof Tabs> = (args) => <Tabs {...args} />;
 export const Primary = Template.bind({});
 
 const tabs = [{ label: "All" }, { label: "Products" }, { label: "Rewards" }, { label: "Assets" }];
 
 Primary.args = {
-  defaultActiveTab: "All",
   tabs,
-  children: (
-    <>
-      <div className="flex justify-between">
-        <h1>hello world</h1>
-        <div className="flex items-center space-x-4">
-          {tabs.map((tab, index) => (
-            <Tab key={index} label={tab.label} />
-          ))}
-        </div>
-      </div>
-      <div className="mt-5">
-        <TabPanelTemplate whenActive="All">Tab 1 Content</TabPanelTemplate>
-        <TabPanelTemplate whenActive="Products">Tab 2 Content</TabPanelTemplate>
-        <TabPanelTemplate whenActive="Rewards">Tab 3 Content</TabPanelTemplate>
-        <TabPanelTemplate whenActive="Assets">Tab 4 Content</TabPanelTemplate>
-      </div>
-    </>
-  ),
+  activeTab: { label: "All" },
+  setActiveTab: (value) => console.log(value),
 };
