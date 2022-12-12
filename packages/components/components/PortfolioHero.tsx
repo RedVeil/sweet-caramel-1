24edcacddf684fffc884f6e316bf9b01346d209e
David Aji <davidenochaji@gmail.com>
2022-12-12 09:37:47 +0100

    update mobile responsiveness for portfolio hero

--- packages/components/components/PortfolioHero.tsx
+++ packages/components/components/PortfolioHero.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { ArrowSmallUpIcon, ChevronDownIcon } from "@heroicons/react/24/solid";
+import { ArrowSmallUpIcon, ArrowLongUpIcon } from "@heroicons/react/24/solid";
 import TabSwitcher from "./Tabs";
 import { Menu } from "@headlessui/react";
 import Dropdown from "./Dropdown";
 import HeroBg from "../stories/assets/portfolioHeroBg.svg";
+import HeroBgMobile from "../stories/assets/portfolioHeroBgmobile.svg";
 
 interface PortfolioHeroProps {
   title: string;
@@ -18,40 +19,47 @@ const PortfolioHero: React.FC<PortfolioHeroProps> = ({ title, NetworkSwitcher })
   ];
   const [selectedOption, setSelectedOption] = useState({ id: "1", value: "50" });
   return (
-    <div className="bg-warmGray bg-opacity-[15%] flex justify-between px-8 pt-10 pb-[14px] relative">
+    <div className="bg-warmGray md:bg-opacity-[15%] flex flex-col md:flex-row justify-between px-8 pt-10 pb-16 md:pb-[14px] relative">
       <div className="relative z-20">
-        <h1 className="text-4xl font-normal m-0 leading-11 mb-4">Your Portfolio Overview</h1>
-        <p className=" text-base text-primaryDark">
+        <h1 className="text-3xl md:text-4xl font-normal m-0 leading-[38px] md:leading-11 mb-4">
+          Your Portfolio Overview
+        </h1>
+        <p className="text-base text-primaryDark">
           A glance at your current Popcorn portfolio <br />
           across different networks.
         </p>
-        {NetworkSwitcher}
+        <div className="hidden md:block">{NetworkSwitcher}</div>
       </div>
-      <div className="absolute bottom-0 left-32">
+      <div className="absolute bottom-0 left-32 hidden md:block">
         <img src={HeroBg} alt="" className="w-full" />
       </div>
+      <div className="absolute right-5 top-16 md:hidden">
+        <img src={HeroBgMobile} alt="" className="w-full" />
+      </div>
       <div>
-        <div className="grid grid-cols-12 gap-8">
-          <div className="col-span-3">
-            <p className="leading-6 text-base mb-2">Weekly P&L</p>
-            <div className="rounded-lg bg-customLightGreen px-4 py-2 text-base font-medium text-white">
-              + 20% <ArrowSmallUpIcon className="w-6" />{" "}
+        <div className="grid grid-cols-12 gap-4 md:gap-8 mt-8 md:mt-0">
+          <div className="col-span-5 md:col-span-3">
+            <p className="leading-6 text-base md:mb-2 font-light md:font-normal">Weekly P&L</p>
+            <div className="md:rounded-lg md:bg-customLightGreen md:px-4 md:py-2 text-3xl md:text-base font-light md:font-medium text-customLightGreen md:text-white flex">
+              <p> +20%</p> <ArrowSmallUpIcon className="w-6 hidden md:inline" />{" "}
+              <ArrowLongUpIcon className="w-5 md:hidden" />{" "}
             </div>
           </div>
-          <div className="col-span-3">
-            <p className="leading-6 text-base">Deposits</p>
-            <p className="text-3xl font-medium">$81K</p>
+          <div className="col-span-5 md:col-span-3">
+            <p className="leading-6 text-base font-light md:font-normal">Deposits</p>
+            <p className="text-3xl font-light md:font-medium">$81K</p>
           </div>
-          <div className="col-span-3">
-            <p className="leading-6 text-base">Vesting</p>
-            <p className="text-3xl font-medium">$81K</p>
+          <div className="col-span-5 md:col-span-3">
+            <p className="leading-6 text-base font-light md:font-normal">Vesting</p>
+            <p className="text-3xl font-light md:font-medium">$81K</p>
           </div>
-          <div className="col-span-3">
-            <p className="leading-6 text-base">POP In Wallet</p>
-            <p className="text-3xl font-medium">$81K</p>
+          <div className="col-span-5 md:col-span-3">
+            <p className="leading-6 text-base font-light md:font-normal">POP In Wallet</p>
+            <p className="text-3xl font-light md:font-medium">$81K</p>
           </div>
         </div>
-        <div className="flex flex-col items-end mt-16">
+        <div className="md:hidden">{NetworkSwitcher}</div>
+        <div className="hidden md:flex flex-col items-end mt-16">
           <TabSwitcher tabs={tabs} defaultActiveTab="All" />
           <div className="mt-9 relative">
             <Dropdown
--- packages/components/stories/PortfolioHero.stories.tsx
+++ packages/components/stories/PortfolioHero.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import PortfolioHero from "../components/PortfolioHero";
 import EthIcon from "./assets/ethereum.svg";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
 
 export default {
   title: "Components/PortfolioHero",
@@ -31,19 +32,25 @@ const networks = [
   },
 ];
 const NetworkSwitcher = (
-  <div className="hidden md:flex flex-row items-center space-x-2 mt-6">
-    {networks.map(({ label, handleClick, isActive }) => (
-      <button
-        key={label}
-        className={`leading-8 h-12 w-18 bg-white border border-customLightGray rounded-3xl text-primary flex justify-center items-center ${
-          isActive ? "border-1 border-tokenTextGray" : "border-customLightGray"
-        }`}
-        type="button"
-        onClick={() => handleClick()}
-      >
-        <img src={EthIcon} alt={label} className="w-4 h-4 object-contain" />
-      </button>
-    ))}
+  <div>
+    <div className="hidden md:flex flex-row items-center space-x-2 mt-6">
+      {networks.map(({ label, handleClick, isActive }) => (
+        <button
+          key={label}
+          className={`leading-8 h-12 w-18 bg-white border border-customLightGray rounded-3xl text-primary flex justify-center items-center ${
+            isActive ? "border-1 border-tokenTextGray" : "border-customLightGray"
+          }`}
+          type="button"
+          onClick={() => handleClick()}
+        >
+          <img src={EthIcon} alt={label} className="w-4 h-4 object-contain" />
+        </button>
+      ))}
+    </div>
+    <button className="w-full py-3 px-5 flex flex-row items-center justify-between space-x-1 rounded-4xl border border-gray-300 bg-white mt-8">
+      <div className="flex items-center">All Networks</div>
+      <ChevronDownIcon className="w-5 h-5" aria-hidden="true" />
+    </button>
   </div>
 );
 
@@ -52,7 +59,3 @@ Primary.args = {
   title: "Assets",
   NetworkSwitcher,
 };
-
-{
-  /*  */
-}
