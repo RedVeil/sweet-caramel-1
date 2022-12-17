538c3a864473a4f9cf9bc199239865a0b27f14ed
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2022-12-17 17:02:14 +0100

    chore: remove unused code

--- packages/components/components/Tabs.tsx
+++ packages/components/components/Tabs.tsx
@@ -1,27 +1,8 @@
-import { useState, createContext, useContext, Dispatch, SetStateAction, FC } from "react";
+import { FC } from "react";
 import classnames from "classnames";
-
-const removeSpaces = (word: string) => {
-  return word.replace(/\s/g, "_");
-};
-
-const TabSwitcherContext = createContext({} as [string, Dispatch<SetStateAction<string>>]);
-
 interface Tab {
   label: string;
 }
-
-// interface TabSwitcherProps {
-//   children: React.ReactNode;
-//   tabs: Tabs[];
-//   defaultActiveTab: string;
-// }
-
-// const TabSwitcher: FC<TabSwitcherProps> = ({ children, tabs, defaultActiveTab }) => {
-//   const [activeTab, setActiveTab] = useState(defaultActiveTab ?? "");
-//   return <TabSwitcherContext.Provider value={[activeTab, setActiveTab]}>{children}</TabSwitcherContext.Provider>;
-// };
-
 interface TabProps {
   tabs: Tab[];
   activeTab: { label: string };
@@ -49,25 +30,3 @@ export const Tabs: FC<TabProps> = ({ tabs, activeTab, setActiveTab }) => {
     </div>
   );
 };
-
-// interface TabPanelProps {
-//   whenActive: string;
-//   children: React.ReactNode;
-// }
-
-// const TabPanel: FC<TabPanelProps> = ({ whenActive, children }) => {
-//   const [activeTab] = useContext(TabSwitcherContext);
-//   return (
-//     <div
-//       className={classnames({
-//         hidden: activeTab !== whenActive,
-//       })}
-//     >
-//       {children}
-//     </div>
-//   );
-// };
-
-// export default TabSwitcher;
-// export { TabPanel, Tab };
-//  export default Tab
