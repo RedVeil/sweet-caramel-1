9126213f1b3bd2ed1bfc9959e7f0c922555d2510
D3Portillo <d3portillo@gmail.com>
2022-12-28 19:41:38 +0100

    fix(numbers.ts): add hotfix percentage taking _hex raw number value from ethers#bignumber

--- packages/components/lib/utils/numbers.ts
+++ packages/components/lib/utils/numbers.ts
@@ -1,8 +1,10 @@
 import { BigNumber, constants } from "ethers";
 
 const ZERO = constants.Zero.toString();
-const HUNDRED = constants.Zero.add(100);
-
 export const getPercentage = (total: BigNumber | undefined, partial: BigNumber | undefined) => {
-  return total?.gt(0) && partial?.gt(0) ? HUNDRED.mul(partial).div(total).toString() : ZERO;
+  return total?.gt(0) && partial?.gt(0)
+    ? parseFloat((100 * ((partial._hex as any) / (total._hex as any))) as any).toLocaleString(undefined, {
+        maximumFractionDigits: 2,
+      })
+    : ZERO;
 };
