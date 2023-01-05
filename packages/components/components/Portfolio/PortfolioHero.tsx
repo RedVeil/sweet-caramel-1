0aea82b7baa35f9a07d8c18e8d3c0d76b0b949a7
D3Portillo <d3portillo@gmail.com>
2023-01-05 16:28:40 +0100

    fix(PorfolioHero.tsx): replace tw class z-1->z-[1]

--- packages/components/components/Portfolio/PortfolioHero.tsx
+++ packages/components/components/Portfolio/PortfolioHero.tsx
@@ -27,7 +27,7 @@ const PortfolioHero: React.FC<PortfolioHeroProps> = ({
 }) => {
   return (
     <div className="bg-warmGray overflow-hidden md:bg-opacity-[15%] flex flex-col md:flex-row justify-between px-8 pt-10 pb-16 md:pb-[14px] relative -mt-5">
-      <div className="relative z-1">
+      <div className="relative z-[1]">
         <h1 className="text-3xl md:text-4xl font-normal m-0 leading-[38px] md:leading-11 mb-4">
           Your Portfolio Overview
         </h1>
